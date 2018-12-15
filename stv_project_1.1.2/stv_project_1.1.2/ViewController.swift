49f23f26c44e7a0c97efcb0353bd3d05eb7dc0e7
YONEDADAYONE <yoneda.dayone8@gmail.com>
2018-12-15 21:49:04 +0900

    課 題stv_project_1.1.2 完了しました

--- stv_project_1.1.2/stv_project_1.1.2/ViewController.swift
+++ stv_project_1.1.2/stv_project_1.1.2/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         call()
-        print("再提出")
+        print("abc")
     }
     
     
