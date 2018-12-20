32f5925b1b9dbe7b4e7384a03c2e0047896dbf15
YONEDADAYONE <yoneda.dayone8@gmail.com>
2018-12-20 22:18:12 +0900

    課 題stv_project_1.1.1再提出です。

--- stv_project_1.1.1/stv_project_1.1.1/stv_project_1.1.1/AppDelegate.swift
+++ stv_project_1.1.1/stv_project_1.1.1/stv_project_1.1.1/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  stv_project_1.1.1
 //
-//  Created by 米田大弥 on 2018/12/13.
+//  Created by 米田大弥 on 2018/12/20.
 //  Copyright © 2018年 hiroya. All rights reserved.
 //
 
@@ -13,16 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
--- stv_project_1.1.1/stv_project_1.1.1/stv_project_1.1.1/ViewController.swift
+++ stv_project_1.1.1/stv_project_1.1.1/stv_project_1.1.1/ViewController.swift
@@ -2,33 +2,27 @@
 //  ViewController.swift
 //  stv_project_1.1.1
 //
-//  Created by 米田大弥 on 2018/12/13.
+//  Created by 米田大弥 on 2018/12/20.
 //  Copyright © 2018年 hiroya. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    let name = "名前"
-    let number = 10
-    let decimal = 0.01
-    let deliberation = true
-    
-    let users: [String: String] = ["apple": "りんご", "grape": "ぶどう", "peach": "桃"]
-    
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    
+    let name: String = "名前"
+    let number: Int = 10
+    let decimal: Double = 0.01
+    let deliberation: Bool = true
+    let user: [String: String] = ["apple": "りんご", "grape": "ぶどう", "peach": "桃"]
+    let numbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         print(name)
         print(number)
         print(decimal)
         print(deliberation)
-        print(users)
+        print(user)
         print(numbers)
         print(numbers[3])
     }
-    
 }
