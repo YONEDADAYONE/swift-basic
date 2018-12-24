b08b51b67ecf532f2f7c4ca0bd410b9c3993da33
YONEDADAYONE <yoneda.dayone8@gmail.com>
2018-12-25 08:38:46 +0900

    課 題stv_project_2.1.18 完了しました

---
+++ stv_project_2.1.17/stv_project_2.1.17/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  stv_project_2.1.17
+//
+//  Created by 米田大弥 on 2018/12/24.
+//  Copyright © 2018年 hiroya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func tapBack(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
