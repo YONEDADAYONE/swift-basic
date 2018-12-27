a45de73d6ebf609ca18ca5a89b986ed05251afee
YONEDADAYONE <yoneda.dayone8@gmail.com>
2018-12-27 15:23:56 +0900

    課 題stv_project_1.1.1 訂正しました

--- stv_project_1.1.1/stv_project_1.1.1/stv_project_1.1.1/ViewController.swift
+++ stv_project_1.1.1/stv_project_1.1.1/stv_project_1.1.1/ViewController.swift
@@ -4,19 +4,18 @@
 //
 //  Created by 米田大弥 on 2018/12/20.
 //  Copyright © 2018年 hiroya. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-    let name: String = "名前"
-    let number: Int = 10
-    let decimal: Double = 0.01
-    let deliberation: Bool = true
+    let name = "名前"
+    let number = 10
+    let decimal = 0.01
+    let deliberation = true
 
-    let users: [String: String] = ["apple": "りんご", "grape": "ぶどう", "peach": "桃"]
+    let fruits  = ["apple": "りんご", "grape": "ぶどう", "peach": "桃"]
     
-    let numbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let numbers  = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +23,7 @@ class ViewController: UIViewController {
         print(number)
         print(decimal)
         print(deliberation)
-        print(users)
+        print(fruits)
         print(numbers)
         print(numbers[3])
     }
